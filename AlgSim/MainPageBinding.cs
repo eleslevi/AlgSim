a8645cb8b6d624a894f2df3477ea61aae2b9c8aa
eleslevi <116953818+eleslevi@users.noreply.github.com>
2023-03-25 14:50:17 +0100

    c1

---
+++ AlgSim/MainPageBinding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AlgSim
+{
+    public class MainPageBinding : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand Sum { get; set; }
+        public ICommand Decision { get; set; }
+        public ICommand Copy { get; set; }
+        public ICommand Selection { get; set; }
+        public ICommand SimpleSwap { get; set; }
+        public ICommand MinimumSelection { get; set; }
+    }
+}
