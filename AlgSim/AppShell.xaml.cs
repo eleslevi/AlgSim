d9a23f3824168fb799e04a921952bc8f14eb3e8a
eleslevi <116953818+eleslevi@users.noreply.github.com>
2023-05-30 22:51:46 +0200

    fix merge issues

--- AlgSim/AppShell.xaml.cs
+++ AlgSim/AppShell.xaml.cs
@@ -4,38 +4,31 @@ using AlgSim.ViewModel;
 namespace AlgSim;
 
 public partial class AppShell : Shell
-
 {
-
 	private MainViewModel _mainViewModel;
 	private SumPage_ViewModel _sumPageViewModel;
 	private MaxSelection_ViewModel _maxSelectionViewModel;
-<<<<<<< Updated upstream
 	private CopyPage_ViewModel _copyPageViewModel;
-=======
     private Intersection_ViewModel _intersectionViewModel;
     private Union_ViewModel _UnionViewModel;
     private Swap_ViewModel _SwapViewModel;
     private MinSelection_ViewModel _MinSelectionViewModel;
->>>>>>> Stashed changes
 
-	{
+    public AppShell(MainViewModel mainViewModel)
+    {
 		InitializeComponent();
 
 		_mainViewModel = mainViewModel;
 		_sumPageViewModel = new SumPage_ViewModel();
 		_maxSelectionViewModel = new MaxSelection_ViewModel();
-<<<<<<< Updated upstream
 		_copyPageViewModel = new CopyPage_ViewModel();
-=======
 		_intersectionViewModel = new Intersection_ViewModel();
 		_UnionViewModel = new Union_ViewModel();
 		_SwapViewModel = new Swap_ViewModel() ;
 		_MinSelectionViewModel = new MinSelection_ViewModel();
->>>>>>> Stashed changes
 
-
-	}
+        _mainViewModel.MenuClicked += new EventHandler(ViewModel_MenuClicked);
+    }
 
 	private void ViewModel_MenuClicked(Object? sender, EventArgs e)
 	{
@@ -47,11 +40,8 @@ public partial class AppShell : Shell
 				break;
 			case "MaxSelection": MaxSelection_Clicked();
 				break;
-<<<<<<< Updated upstream
 			case "Copy": Copy_Clicked();
 				break;
-			default: DisplayAlert("Hiányzó tétel", "A tétel még nem elérhető", "OK");
-=======
             case "Intersection": Intersection_Clicked();
                 break;
             case "Union": Union_Clicked();
@@ -61,7 +51,6 @@ public partial class AppShell : Shell
             case "MinSelection": MinSelection_Clicked();
                 break;
             default: DisplayAlert("Hiányzó tétel", "A tétel még nem elérhető", "OK");
->>>>>>> Stashed changes
 				break;
         }
 	}
